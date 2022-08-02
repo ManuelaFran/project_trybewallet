b88b7e8fb75aac9d1eccfbd02faf663d7cc0d75a
Manuela França <manuelaalves26@gmail.com>
2022-08-02 10:32:14 -0300

    Alteração na TableField do requisito 7.

--- src/components/TableField.js
+++ src/components/TableField.js
@@ -14,22 +14,3 @@ export const fields = [
 export function tableFields(elements) {
   return (<th>{elements}</th>);
 }
-
-export function table(element) {
-  return (
-    <tr key={ element.id }>
-      <td>{element.description}</td>
-      <td>{element.tag}</td>
-      <td>{element.method}</td>
-      <td>{Number(element.value).toFixed(2)}</td>
-      <td>{element.exchangeRates[element.currency].name}</td>
-      <td>{Number(element.exchangeRates[element.currency].ask).toFixed(2)}</td>
-      <td>
-        {Number(element.value * element.exchangeRates[element.currency].ask)
-          .toFixed(2)}
-
-      </td>
-      <td>Real</td>
-    </tr>
-  );
-}
